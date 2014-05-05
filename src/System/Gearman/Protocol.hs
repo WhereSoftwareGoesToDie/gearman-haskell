6419aa929d642a4518a4d6f9ca57f84981359831
Sharif Olorin <sio@tesser.org>
2014-05-05 17:28:13 +1000

    And I don't think we need the NULL at the start

--- src/System/Gearman/Protocol.hs
+++ src/System/Gearman/Protocol.hs
@@ -252,7 +252,7 @@ packData d          = runPut $ do
     putWord32be $ fromIntegral $ S.length (toPayload d)
     putLazyByteString $ toPayload d
   where
-    toPayload = S.append "\0" . (S.intercalate "\0")
+    toPayload = S.intercalate "\0"
 
 buildEchoReq        :: [S.ByteString] -> S.ByteString
 buildEchoReq        = (S.append (renderHeader echoReq)) . packData
