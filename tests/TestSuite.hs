d03498da0970926213e694b9e67e16b85a380fbf
Sharif Olorin <sio@tesser.org>
2014-05-06 17:26:37 +1000

    Half a testcase

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module TestSuite where
 
 import Control.Monad
@@ -8,14 +10,21 @@ import Data.Maybe
 import Test.Hspec 
 import Test.Hspec.HUnit
 import Test.HUnit
+import qualified Data.ByteString.Char8 as S
 
 import System.Gearman
 import System.Gearman.Connection
 import System.Gearman.Protocol
+import System.Gearman.Worker
 
 testConnectivity = do
     c <- testConnectEcho "localhost" "4730"
     assertBool "connect failed" (isNothing c)
+    return ()
+
+testRegister = do
+    r <- runGearman "localhost" "4370" $ return $ addFunc "reverse" rvs Nothing
+    assertBool "send failed" (isNothing r)
     return ()
 
 suite :: Spec
